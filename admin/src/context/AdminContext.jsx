cd4c532e9b4667f3334620931b2844ae1ea5628e
Sandeep0528 <153294025+Sandeep0528@users.noreply.github.com>
2025-04-19 14:08:57 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,7 +9,7 @@ const AdminContextProvider = (props) => {
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://prescripto-appoint.onrender.com"
 
     const getAllDoctors = async () => {
         try {
